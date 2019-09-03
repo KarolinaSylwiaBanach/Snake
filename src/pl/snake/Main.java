fc84fe00909691cc21d06811b5f7410364691779
karol <karola.024@wp.pl>
2019-09-03 09:45:22 +0200

    create a frame

---
+++ src/pl/snake/Main.java
@@ -0,0 +1,23 @@
+package pl.snake;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame();
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int sizeWidth = 905;
+        int sizeHeight = 700;
+        int width = (dimension.width- sizeWidth) /2;
+        int height = (dimension.height- sizeHeight) /2;
+        frame.setBounds(width,height,sizeWidth,sizeHeight);
+        frame.setBackground(Color.WHITE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
